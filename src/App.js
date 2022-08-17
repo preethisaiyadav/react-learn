d3049cbf63c01d56523f9c5507e1db0a880569c0
preethisaiyadav <dpreethi703@gmail.com>
2022-08-17 11:20:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react'
-/* import Message from './Message/Message' */
-import Navbar from './Navbar/Navbar'
-import Employee from './Employee/Employee'
-import Counter from './Counter/Counter'
-class App extends React.Component {
-    render() {
-        return <div>
-            <Navbar />
-            <h1>Employee Component</h1>
-            <hr />
+import React  ,{useState} from 'react'
+ const App =()=>{
+    const [Data,setData]=useState({
+        email:'',
+        password:''
 
-            <Employee />
-            {/* <Counter /> */}
-        </div>
+    })
+const changeHandler =e=>{
+    setData({...Data,[e.target.name]:e.target.value})
+
+}
+const submitHandler=e=>{
+    e.preventDefault();   
+    if(Data.password.length<5){
+        alert('provide password length>5')
+    }
+    else{
+        console.log(Data);
     }
 }
-export default App
+    return(
+        
+        <div style={{display:"flex" ,justifyContent:"center"}}>
+        <form onSubmit={submitHandler}>
+                <label>E-mail : </label>
+                <input type="text"name="email"/> <br/>
+                <label>Password :</label> 
+                <input type="password"name="password"/> <br/>
+                    <input type="submit"value="login"/> 
+                
+            </form>
+        </div>
+    )
+ }
+ export default App
