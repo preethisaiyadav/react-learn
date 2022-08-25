b6b29ff3e7dead349a31e911d3789e895aa646fe
preethisaiyadav <dpreethi703@gmail.com>
2022-08-25 20:40:07 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Navbar from './Navbar/Navbar'
-import Salary from './FormEx/Eventhandling/Salary'
-import Login from './FormEx/Login'
-import Employee from './Empolyee/Employee'
 
-
- class App extends React.Component  {
-  render(){
-  return (
-    <div>
-         <Navbar/>
-   <h1>Login Details</h1>
-   <hr/> 
-     <Login/>
-     <Salary/>
-      <Employee/>
-
-    </div>
-   
-  )
-  }
+import ContactApp from './ContactApp/ContactApp'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+class App extends Component {
+    render() {
+        return <React.Fragment>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/contacts" element={<ContactApp />} />
+                    
+                </Routes>
+            </Router>
+        </React.Fragment>
+    }
 }
+
 export default App
+
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 class Navbar extends Component {
     render() {
-        return <nav className="navbar navbar-dark bg-dark">
-            <a href="#" className="navbar-brand">React</a>
-            <a href='#'className='navbar-barnd'>form details</a>
-            <a href='#'className='navbar-barnd'>Empolyee details</a>
+        return <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <Link className="navbar-brand" to='/'>Contact App </Link>
+            <div className="ml-auto">
+                <ul className="navbar-nav">
+                    <li className="nav-list">
+           
+                <li className='nav-list'></li><Link className="nav-link" to="/contacts">Contactlist</Link>
+                        </li>
+                </ul>
+            </div>
         </nav>
-
     }
 }
 
